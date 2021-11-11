b398b43bd4c7bf66b4ae5c3c40ab91588407ea89
noliynyk <n.oliynyk@seclgroup.com>
2021-11-11 16:16:02 +0300

    fonts added. fully Typography component done

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,6 @@ import FavoriteIcon from '../src/shared/icons/Favorite/Favorite'
 import BaseButton from '../src/shared/ui/BaseButton/BaseButtons'
 import Typography from '../src/shared/ui/Typography/Typography'
 
-
-
 const Home: NextPage = () => {
 
   return (
@@ -19,9 +17,14 @@ const Home: NextPage = () => {
         <BaseButton type="blank">На главную</BaseButton>
         <BaseButton type="secondary" icon={<FavoriteIcon />} />
       </div>
-      <div style={{display: 'flex', gap: '15px', alignItems: 'center'}}>
+      <div style={{display: 'flex', gap: '15px', alignItems: 'center', flexDirection: 'column'}}>
         <h3>Typography:</h3>
-        <Typography />
+        <Typography size={'small'}> Small text </Typography>
+        <Typography size={'default'} color="accent"> Default text with accent color</Typography>
+        <Typography size={'medium'} color='nude' weight="bold"> Medium text with nude color & bold weight</Typography>
+        <Typography size={'subheader'} color='tertiary' weight="bold"> Subheader text with tertiary color</Typography>
+        <Typography size={'big'} color='nude'> Big text with nude color (price example) - 30 000 000 $</Typography>
+        <div style={{backgroundColor: "#000"}}><Typography size={'header'} color='secondary'> Header text with secondary color (white)</Typography></div>
       </div>
     </div>
   )
--- src/shared/ui/Typography/Typography.tsx
+++ src/shared/ui/Typography/Typography.tsx
@@ -1,7 +1,19 @@
-export default function Typography() {
+import classNames from "classnames"
+import { IPropsGeneral } from "../../lib/interfaces/general"
+import s from './Typography.module.scss' 
+
+interface Props extends IPropsGeneral {
+    color?: "default" | "nude" | "accent" | "secondary" | "tertiary",
+    size?: "default" | "subheader" | "header" | "small" | "medium" | "big",
+    weight?: "light" | "regular" | "medium" | "bold" 
+}
+
+const Typography:React.FC<Props> = ({className, color = 'default', size = 'default', weight = 'regular', children}) => {
     return (
-        <p>
-           kek
+        <p className={classNames(s.general, s[`color_${color}`], s[`weight_${weight}`], s[`size_${size}`], className)}>
+           {children}
         </p>
     )
 }
+
+export default Typography
