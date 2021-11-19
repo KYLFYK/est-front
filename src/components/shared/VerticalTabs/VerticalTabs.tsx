1154fa8d7c49c76f3ee5d071c5dffdb00e2192db
noliynyk <n.oliynyk@seclgroup.com>
2021-11-19 15:56:51 +0300

    Vertical Tabs Improvemets

--- src/components/shared/VerticalTabs/VerticalTabs.tsx
+++ src/components/shared/VerticalTabs/VerticalTabs.tsx
@@ -1,33 +1,23 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Typography from '../Typography/Typography';
 import css from './VerticalTabs.module.scss'
 
-function searchMenu(num: number) {
-    switch (num) {
-        case 1:
-            return <div>1</div>
-        case 2:
-            return <div>2</div>
-        case 3:
-            return <div>3</div>
-        case 4:
-            return <div>4</div>
-        case 5:
-            return <div>5</div>
-        default:
-            return <div>1</div>
-    }
+interface ITabItem {
+    title: string,
+    Component: JSX.Element
+}
+interface Props {
+    tabs: ITabItem[]
 }
 
-
-const VerticalTabs = () => {
+const VerticalTabs: React.FC<Props> = ({tabs}) => {
     const [active, setActive] = useState<number>(1)
     return (
         <div className={css.body}>
-            <MenuUser active={active} onActive={setActive} menu={['Агенства','Застройщики','Собственники','Статистика']}/>
+            <MenuUser active={active} onActive={setActive} menu={tabs.map((tab) => tab.title)} />
             <div className={css.information}>
                 {
-                    searchMenu(active)
+                   tabs[active].Component
                 }
             </div>
 
@@ -39,19 +29,19 @@ const VerticalTabs = () => {
 type MenuUserType = {
     active: number
     onActive: (num: number) => void
-    menu:Array<string>
+    menu: Array<string>
 }
 
-const MenuUser: React.FC<MenuUserType> = ({active, onActive,menu}) => {
+const MenuUser: React.FC<MenuUserType> = ({ active, onActive, menu }) => {
 
     return (
         <div className={css.menu}>
             {
-                menu.map((name,index)=>(
+                menu.map((name, index) => (
                     <div
                         key={index}
                         className={css.menuItem}
-                        style={{color: active === index ? '#C5A28E' : ''}}
+                        style={{ color: active === index ? '#C5A28E' : '' }}
                         onClick={() => onActive(index)}
                     >
                         <Typography size={'default'} color={active === index ? 'nude' : 'tertiary'} weight="bold">
