36f8a04d9352f98776ee478482b94550b27e40b8
noliynyk <n.oliynyk@seclgroup.com>
2021-11-23 18:54:11 +0300

    Mass Media & Activity & Owners Tabs

--- src/components/tabs/Developer/config.ts
+++ src/components/tabs/Developer/config.ts
@@ -1,5 +1,7 @@
 import { IArticleGeneral, IOption } from "../../../utils/interfaces/general";
 import { IDeveloperContactItem } from "./components/Contacts/Contacts";
+import { IDeveloperArticleItem } from "./components/MassMedia/DeveloperMassMedia";
+import { IDeveloperCompanyData } from "./components/Owners/DeveloperOwners";
 
 export const ABOUT_PARAGRAPHS = [
   "Брусника — российская девелоперская компания. Специализируется на строительстве жилых многоэтажных домов. Основана в 2004 году. Штаб-квартира находится в Екатеринбурге. Сегодня Брусника строит современное демократичное жильё в крупных городах Урала и Сибири, в Москве и Московский области. Ежегодно это 6 000 новых квартир для российских семей.",
@@ -109,3 +111,127 @@ export const DEVELOPER_REQUISITS: IOption<IArticleGeneral>[] = [
     },
   },
 ];
+
+export const DEVELOPER_OWNERS_COMPANY: IDeveloperCompanyData = {
+  defaultInfo: [
+    {
+      value: "status",
+      label: {
+        title: "Статус компании",
+        text: "Статус компании",
+      },
+    },
+    {
+      value: "head",
+      label: {
+        title: "Руководитель",
+        text: "Иванов Иван Иванович",
+      },
+    },
+    {
+      value: "owner",
+      label: {
+        title: "Учредители",
+        text: "МЕНЕДЖМЕНТ-ЮБИКС, ООО, РЕБРИК НИКОЛАЙ ЮРЬЕВИЧ",
+      },
+    },
+  ],
+  numericInfo: [
+    {
+      value: "size",
+      label: {
+        title: "Размер предприятия",
+        text: "60 человек",
+      },
+    },
+    {
+      value: "staffAmmount",
+      label: {
+        title: "Численность персонала",
+        text: "60 человек",
+      },
+    },
+    {
+      value: "filials",
+      label: {
+        title: "Филиалы",
+        text: "5",
+      },
+    },
+    {
+      value: "revenue",
+      label: {
+        title: "Выручка",
+        text: "40 010 000 ₽ ",
+      },
+    },
+    {
+      value: "profit",
+      label: {
+        title: "Чистая прибыль",
+        text: "40 010 000 ₽ ",
+      },
+    },
+    {
+      value: "assets",
+      label: {
+        title: "Чистые активы",
+        text: "40 010 000 ₽",
+      },
+    },
+  ],
+};
+
+export const DEVELOPER_OWNER_GOVERMENT: IOption<IArticleGeneral>[] = [
+  {
+    value: "date",
+    label: { title: "Дата регистрации", text: "06.08.2014" },
+  },
+  {
+    value: "authority",
+    label: {
+      title: "Регистрирующий орган",
+      text: "Межрайонная ИФНС России № 5 по Смоленской области",
+    },
+  },
+  {
+    value: "authorityAddress",
+    label: {
+      title: "Адрес регистрирующего органа",
+      text: "214018, Смоленская обл, Смоленск г, Гагарина пр-кт, д 23В",
+    },
+  },
+  {
+    value: "authorityBusiness",
+    label: {
+      title: "Регистрирующий орган, в котором находится регистрационное дело",
+      text:
+        "Межрайонная инспекция Федеральной налоговой службы № 5 по Смоленской области",
+    },
+  },
+];
+
+export const DEVELOPER_PRIMARY_ACTIVITIES: string[] = [
+  "Деятельность заказчика-застройщика, генерального подрядчика",
+];
+
+export const DEVELOPER_SECONDARY_ACTIVITIES: string[] = [
+  "Передача электроэнергии и технологическое присоединение к распределительным электросетям",
+  "Покупка и продажа собственного недвижимого имущества",
+  "Подготовка к продаже собственного недвижимого имущества",
+  "Аренда и управление собственным или арендованным недвижимым имуществом",
+];
+
+export const DEVELOPER_MASS_MEDIA_ARTICLES: IDeveloperArticleItem[] = Array(
+  5
+).fill({
+  link: "",
+  date: new Date(),
+  title:
+    "«Брусника» выпустит облигации на 4 млрд ₽ ради земли и перевооружения",
+  description:
+    "Девелоперская компания Брусника объявила о дебютном выпуске облигаций объемом до 4 млрд руб. на...",
+  icon:
+    "https://pbs.twimg.com/profile_images/984033833900298245/5U8I3xfZ_400x400.jpg",
+  id: "1",
+});
--- src/components/tabs/Developer/index.tsx
+++ src/components/tabs/Developer/index.tsx
@@ -1,9 +1,12 @@
 import React from "react"
 import VerticalTabs from "../../shared/VerticalTabs/VerticalTabs"
 import DeveloperAbout from "./components/About/About"
+import DeveloperActivity from "./components/Activity/DeveloperActivity"
 import DeveloperContacts from "./components/Contacts/Contacts"
+import DeveloperMassMedia from "./components/MassMedia/DeveloperMassMedia"
+import DeveloperOwners from "./components/Owners/DeveloperOwners"
 import DeveloperRequisites from "./components/Requisites/DeveloperRequisites"
-import { ABOUT_PARAGRAPHS, DEVELOPER_CONTACTS, DEVELOPER_REQUISITS } from "./config"
+import { ABOUT_PARAGRAPHS, DEVELOPER_CONTACTS, DEVELOPER_MASS_MEDIA_ARTICLES, DEVELOPER_OWNERS_COMPANY, DEVELOPER_OWNER_GOVERMENT, DEVELOPER_PRIMARY_ACTIVITIES, DEVELOPER_REQUISITS, DEVELOPER_SECONDARY_ACTIVITIES } from "./config"
 
 const DeveloperTabs: React.FC<{}> = () => {
     return (
@@ -11,7 +14,11 @@ const DeveloperTabs: React.FC<{}> = () => {
             tabs={[
                 { title: "О застройщике", Component: <DeveloperAbout paragraphs={ABOUT_PARAGRAPHS} /> },
                 { title: "Контакты", Component: <DeveloperContacts items={DEVELOPER_CONTACTS} /> },
-                { title: "Реквизиты", Component: <DeveloperRequisites items={DEVELOPER_REQUISITS}/> }
+                { title: "Реквизиты", Component: <DeveloperRequisites items={DEVELOPER_REQUISITS}/> },
+                { title: "Учредители", Component: <DeveloperOwners companyData={DEVELOPER_OWNERS_COMPANY} govermentData={DEVELOPER_OWNER_GOVERMENT}/> },
+                { title: "Виды деятельности", Component: <DeveloperActivity primaryActivities={DEVELOPER_PRIMARY_ACTIVITIES} secondaryActivities={DEVELOPER_SECONDARY_ACTIVITIES}/> },
+                { title: "СМИ о застройщике", Component: <DeveloperMassMedia articlesItems={DEVELOPER_MASS_MEDIA_ARTICLES}/> },
+                
             ]}
         />
     )
