c0cb583499857c65aafacdec10e7428e1d185bce
noliynyk <n.oliynyk@seclgroup.com>
2021-11-24 16:56:13 +0300

    Storybook Tabs fix

--- src/components/tabs/Developer/index.stories.tsx
+++ src/components/tabs/Developer/index.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import DeveloperTabs from '.';
+import { OBJECT_DEVELOPER_TABS_DATA } from '../../containers/ObjectDeveloper/config';
 
 export default {
     title: 'Tabs/Developer',
@@ -12,6 +13,6 @@ export default {
 const Template: ComponentStory<typeof DeveloperTabs> = (args) => <DeveloperTabs {...args} />;
 export const Example = Template.bind({});
 Example.args = {
-
+    tabsData: OBJECT_DEVELOPER_TABS_DATA
 };
 
