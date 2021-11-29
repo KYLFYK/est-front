4afc9f02f59d2910fbac02dbf3f8dfa70fdba542
Aleksandr <zybkob@f-case.ru>
2021-11-29 15:55:42 +0300

    merge

--- src/components/containers/AgentsContainer/AgentBlock/AgentBlock.stories.tsx
+++ src/components/containers/AgentsContainer/AgentBlock/AgentBlock.stories.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { AgentBlock } from './AgentBlock';
 
-
 export default {
     title: 'Components/AgentsContainer',
     component: AgentBlock,
