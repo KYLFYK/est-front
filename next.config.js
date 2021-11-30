0a429aadaf4a5c1befbb728a0c81154ef4069268
Nickolay <arbuzov.n@f-case.ru>
2021-11-30 14:31:33 +0300

    feat: update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['cdn.pixabay.com', 'images.unsplash.com', 'images.ctfassets.net'],
+  },
 }
