fc9281ff95e4a331abbf439580d03499f7cffa1c
Aleksandr <zybkob@f-case.ru>
2021-12-17 14:23:57 +0300

    fix-tours

--- src/components/containers/ToursContainer/ToursContainer.tsx
+++ src/components/containers/ToursContainer/ToursContainer.tsx
@@ -5,12 +5,12 @@ import {VRTour} from "./Tours/VRTour/VRTour";
 import s from './ToursContainer.module.scss'
 
 type ToursContainerType = {
-    Online_tour:{
+    Online_tour?:{
         threeD_tour:{
-            url? :string,
+            url :string,
         },
         vr_tour:{
-            url? :string,
+            url :string,
         }
     },
 }
@@ -19,8 +19,8 @@ const ToursContainer :FC<ToursContainerType> = ({ Online_tour }) => {
     return (
         <div className={s.container}>
             <VerticalTabs className={s.padding} tabs={[
-                {title:'3D тур',Component:<ThreeDTour url={Online_tour.threeD_tour.url} />},
-                {title:'VR тур',Component:<VRTour url={Online_tour.vr_tour.url} />},
+                {title:'3D тур',Component:<ThreeDTour url={Online_tour && Online_tour.threeD_tour.url  } />},
+                {title:'VR тур',Component:<VRTour url={Online_tour && Online_tour.vr_tour.url} />},
             ]} />
         </div>
     );
