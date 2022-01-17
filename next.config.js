ecfdafbedf990049ed29dd3da620dad28e130443
Aleksandr <zybkob@f-case.ru>
2022-01-17 13:38:42 +0300

    disable eslint

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ module.exports = {
     domains: ['cdn.pixabay.com', 'images.unsplash.com', 'images.ctfassets.net'],
     loader: 'custom'
   },
+
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
