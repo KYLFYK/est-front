2d279c15e186f0ceb26be47d5048d9b7c04baa19
Aleksandr <zybkob@f-case.ru>
2022-01-18 11:52:24 +0300

    developer-cabinet Mobx

--- src/components/tabs/Account/Developer/DeveloperRoleTabs.tsx
+++ src/components/tabs/Account/Developer/DeveloperRoleTabs.tsx
@@ -1,18 +1,21 @@
-import React, { FC } from "react";
+import React, {FC} from "react";
 import VerticalTabs from "../../../shared/VerticalTabs/VerticalTabs";
-import { PersonalArea } from "./components/PersonalArea";
-import { Notifications } from "./components/Notifications";
-import { MyObjects } from "./components/MyObjects";
+import {PersonalArea} from "./components/PersonalArea";
+import {Notifications} from "./components/Notifications";
+import {MyObjects} from "./components/MyObjects";
+import {MainContainer} from "../../../containers/MainContainer/MainContainer";
 
 export const DeveloperRoleTabs: FC = () => {
-  return (
-    <VerticalTabs
-        link={true}
-      tabs={[
-        { title: "Личный кабинет", Component: <PersonalArea /> },
-        { title: "Мои объекты", Component: <MyObjects /> },
-        { title: "Уведомления", Component: <Notifications /> },
-      ]}
-    />
-  );
+    return (
+        <MainContainer>
+            <VerticalTabs
+                link={true}
+                tabs={[
+                    {title: "Личный кабинет", Component: <PersonalArea/>},
+                    {title: "Мои объекты", Component: <MyObjects/>},
+                    {title: "Уведомления", Component: <Notifications/>},
+                ]}
+            />
+        </MainContainer>
+    );
 };
