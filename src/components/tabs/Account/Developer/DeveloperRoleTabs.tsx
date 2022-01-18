9cac9ac0beeb79813ac88cd12c2038b8cfa4e693
Aleksandr <zybkob@f-case.ru>
2022-01-18 18:10:39 +0300

    developer-myObject Mobx

--- src/components/tabs/Account/Developer/DeveloperRoleTabs.tsx
+++ src/components/tabs/Account/Developer/DeveloperRoleTabs.tsx
@@ -1,11 +1,29 @@
-import React, {FC} from "react";
+import React, {FC, useState} from "react";
 import VerticalTabs from "../../../shared/VerticalTabs/VerticalTabs";
 import {PersonalArea} from "./components/PersonalArea";
 import {Notifications} from "./components/Notifications";
 import {MyObjects} from "./components/MyObjects";
 import {MainContainer} from "../../../containers/MainContainer/MainContainer";
+import AgentsNotifications from "../Agent/components/Notifications/Notifications";
+import {useStoreDeveloperNotificationsStore} from "../../../../mobx/role/developer/notifications/notifications";
+
 
 export const DeveloperRoleTabs: FC = () => {
+    const store = useStoreDeveloperNotificationsStore()
+    const [notification,setNotification]=useState<Array<{id:string,date:string, time:string,message:string, read:boolean}>>(store.initialData.notifications)
+
+    const onReadAll = () => {
+        const newNotification = notification.map(t=>({...t, read:true}))
+        setNotification(newNotification)
+    }
+    const onRead = (number:number) => {
+        const newNotification = notification.map((t,index)=>index===number? {...t,read:true} : t  )
+        setNotification(newNotification)
+    }
+    const deleteNotification = (number:number) =>{
+        const newNotification = notification.filter((t,index)=>index !==number)
+        setNotification(newNotification)
+    }
     return (
         <MainContainer footerColor={"accent"}>
             <VerticalTabs
@@ -13,7 +31,12 @@ export const DeveloperRoleTabs: FC = () => {
                 tabs={[
                     {title: "Личный кабинет", Component: <PersonalArea/>},
                     {title: "Мои объекты", Component: <MyObjects/>},
-                    {title: "Уведомления", Component: <Notifications/>},
+                    {title: "Уведомления", Component: <AgentsNotifications
+                        onRead={onRead}
+                        onReadAll={onReadAll}
+                        notification={notification}
+                        onDelete={deleteNotification}
+                    />},
                 ]}
             />
         </MainContainer>
