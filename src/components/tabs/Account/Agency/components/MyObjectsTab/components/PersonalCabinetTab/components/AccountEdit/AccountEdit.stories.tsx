4abd63d6afff7f5929a786e09f904462c201ced3
Aleksandr <zybkob@f-case.ru>
2022-02-09 16:51:47 +0300

    merge

--- src/components/tabs/Account/Agency/components/MyObjectsTab/components/PersonalCabinetTab/components/AccountEdit/AccountEdit.stories.tsx
+++ src/components/tabs/Account/Agency/components/MyObjectsTab/components/PersonalCabinetTab/components/AccountEdit/AccountEdit.stories.tsx
@@ -7,15 +7,6 @@ export default {
     component: AccountEdit,
 
 } as ComponentMeta<typeof AccountEdit>;
-const infoAgencyMoc = {
-    name: 'Estatum',
-    status: 'Agency',
-    address: 'Смоленская обл. г.Смоленск',
-    phone: '+7 999 777 55 11',
-    email: 'estatum@mail.com',
-    website: 'estatum.com',
-    description: 'You agency'
-}
 
 const Template: ComponentStory<typeof AccountEdit> = (args) => <AccountEdit {...args} />;
 export const AccountEdit_ = Template.bind({});
