e67557153ad4719b6eeea00c4e686d51fd10d44b
Aleksandr <zybkob@f-case.ru>
2022-02-15 11:55:05 +0300

    favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,7 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons&display=swap"
           />
+          <link rel="shortcut icon" href="/favicon.ico" />
           <style jsx global>
             {`
               html,
