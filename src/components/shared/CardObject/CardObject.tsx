6e8a548055ff4ff75e0652cc7529db38251eeace
Maksim <kylfyk@bk.ru>
2022-02-28 18:11:43 +0500

    Fix image

--- src/components/shared/CardObject/CardObject.tsx
+++ src/components/shared/CardObject/CardObject.tsx
@@ -1,5 +1,7 @@
 import React, { FC, ReactNode } from "react";
 import Image from "next/image";
+import { myLoader } from "../../../utils/image/image";
+
 import css from "./CardObject.module.scss";
 
 type CardObjectType = {
@@ -12,12 +14,13 @@ const CardObject: FC<CardObjectType> = ({ children, img, className }) => {
   return (
     <div className={css.card}>
       <Image
+        unoptimized
         src={img}
         height={"126px"}
         width={"229px"}
         className={css.image}
         alt={"photo"}
-        loader={(e) => e.src}
+        loader={(e) => myLoader(e.src, e.width, e.quality)}
       />
       <div style={{ width: "100%" }} className={className}>
         {children}
