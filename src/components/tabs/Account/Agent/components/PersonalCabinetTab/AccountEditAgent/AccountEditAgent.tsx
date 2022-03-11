a74edaf762cb2bbc8f5edb947df3215d6778fcfa
Aleksandr <zybkob@f-case.ru>
2022-03-11 09:55:33 +0300

    merge

--- src/components/tabs/Account/Agent/components/PersonalCabinetTab/AccountEditAgent/AccountEditAgent.tsx
+++ src/components/tabs/Account/Agent/components/PersonalCabinetTab/AccountEditAgent/AccountEditAgent.tsx
@@ -151,7 +151,7 @@ const AccountEditAgent: FC<AccountEditType> = observer(({ onEdit }) => {
   const changeAvatar = (data: FormData) => {
     store.updateAvatar(data,store.initialData.id).then();
   };
-  console.log("store.initialData.file", store.initialData.file[0].url)
+
   return (
     <div>
       <BackPage onBackPage={backPage} title={"Редактирование аккаунта"} />
