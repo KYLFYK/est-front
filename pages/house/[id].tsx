17129f8a7bc75399b0b4954fb0d4218a6476c720
Aleksandr <zybkob@f-case.ru>
2022-04-04 10:17:22 +0300

    env

--- pages/house/[id].tsx
+++ pages/house/[id].tsx
@@ -366,8 +366,8 @@ export default House;
 export async function getServerSideProps({ params }: any) {
   const res = await fetch(
    
-    `${process.env.HOST}${UrlObj.house}/${params.id}`
-    // `https://estatum.f-case.ru/api/${UrlObj.house}/${params.id}`
+    // `${process.env.HOST}${UrlObj.house}/${params.id}`
+    `https://estatum.f-case.ru/api/${UrlObj.house}/${params.id}`
     // `http://marketplace-back:8008/api/${UrlObj.house}/${params.id}`
   );
 
