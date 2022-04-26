8978115d2febb2feee3e82bf0f995e99b6ee719a
Nickolay <arbuzov.n@f-case.ru>
2022-04-27 00:20:21 +0300

    feat: add adaptive mortgage and request

--- src/components/shared/Typography/Typography.tsx
+++ src/components/shared/Typography/Typography.tsx
@@ -1,28 +1,64 @@
-import classNames from "classnames"
-import { IPropsGeneral } from "../../../utils/interfaces/general"
-import s from './Typography.module.scss'
+import classNames from "classnames";
+import { IPropsGeneral } from "../../../utils/interfaces/general";
+import s from "./Typography.module.scss";
 
 interface Props extends IPropsGeneral {
-    color?: "default" | "nude" | "accent" | "secondary" | "tertiary" | "red" | "green" | "disabled" | "altWhite",
-    size?: "default" | "subheader" | "header" | "small" | "medium" | "big" | "headerLow" | "subheaderBig",
-    weight?: "light" | "regular" | "medium" | "bold",
-    icon?: JSX.Element | string | boolean,
-    iconPosition?: "start" | "end"
-    inline?: boolean
+  color?:
+    | "default"
+    | "nude"
+    | "accent"
+    | "secondary"
+    | "tertiary"
+    | "red"
+    | "green"
+    | "disabled"
+    | "altWhite"
+    | "altGray";
+  size?:
+    | "default"
+    | "subheader"
+    | "header"
+    | "small"
+    | "medium"
+    | "big"
+    | "headerLow"
+    | "subheaderBig";
+  weight?: "light" | "regular" | "medium" | "bold";
+  icon?: JSX.Element | string | boolean;
+  iconPosition?: "start" | "end";
+  inline?: boolean;
 }
 
-const Typography: React.FC<Props> = ({onClick, className, color = 'default', size = 'default', weight = 'regular', iconPosition = 'start', icon, inline, children}) => {
-    return (
-        <p 
-            onClick={onClick} 
-            className={classNames(s.general, s[`color_${color}`], s[`weight_${weight}`], s[`size_${size}`], className)} 
-            style={{display: inline ? 'inline-flex' : 'flex'}}
-        >
-            {(icon && iconPosition === 'start') && <span className={s.icon}>{icon}</span>}
-            {children}
-            {(icon && iconPosition === 'end') && <span className={s.icon}>{icon}</span>}
-        </p>
-    )
-}
+const Typography: React.FC<Props> = ({
+  onClick,
+  className,
+  color = "default",
+  size = "default",
+  weight = "regular",
+  iconPosition = "start",
+  icon,
+  inline,
+  children,
+}) => {
+  return (
+    <p
+      onClick={onClick}
+      className={classNames(
+        s.general,
+        s[`color_${color}`],
+        s[`weight_${weight}`],
+        s[`size_${size}`],
+        className
+      )}
+      style={{ display: inline ? "inline-flex" : "flex" }}
+    >
+      {icon && iconPosition === "start" && (
+        <span className={s.icon}>{icon}</span>
+      )}
+      {children}
+      {icon && iconPosition === "end" && <span className={s.icon}>{icon}</span>}
+    </p>
+  );
+};
 
-export default Typography
+export default Typography;
