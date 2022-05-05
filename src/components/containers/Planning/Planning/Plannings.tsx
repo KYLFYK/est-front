e02872260dd323cde271ec43301910365d02ea4a
Maksim <kylfyk@bk.ru>
2022-05-05 18:40:55 +0500

    Fix complex issue

--- src/components/containers/Planning/Planning/Plannings.tsx
+++ src/components/containers/Planning/Planning/Plannings.tsx
@@ -1,68 +1,98 @@
-import React, {useState} from 'react'
-import Link from 'next/link'
-import HeadLine from '../../../shared/HeadLine/HeadLine'
-import SampleImage from '../assets/planning_sample.png'
-import Card from '../Card/Card'
-import s from './Plannings.module.scss'
-import {datetoQuarterFormat} from '../../../../lib/mapping/objectDates'
-import PlanningFilter from "../../../../../src/components/containers/PlanningFilter/PlanningFilter"
+import React, { useState } from "react";
+import Link from "next/link";
+import HeadLine from "../../../shared/HeadLine/HeadLine";
+import Card from "../Card/Card";
+import s from "./Plannings.module.scss";
+import { datetoQuarterFormat } from "../../../../lib/mapping/objectDates";
+import PlanningFilter from "../../../../../src/components/containers/PlanningFilter/PlanningFilter";
+import { DesktopOnly } from "../../Adaptive/DesktopOnly";
+import { useAdaptiveContext } from "../../../../mobx/adaptive/AdaptiveProvider";
+import { AdaptiveParams } from "../../../../mobx/adaptive/store";
+import { MobileOnly } from "../../Adaptive/MobileOnly";
+import BaseButton from "../../../shared/BaseButton/BaseButtons";
 
 // TODO: Take types from 'model' folder, when global state gets its types
 
 interface IObjectPlanningItem {
-    file: {id: number, url: string}[],
-    price: number,
-    name: string,
-    buildingNumber: number,
-    deadline: string,
-    floor: number,
-    id: number,
+  file: { id: number; url: string }[];
+  price: number;
+  name: string;
+  buildingNumber: number;
+  deadline: string;
+  floor: number;
+  id: number;
 }
 
 interface Props {
-    FilterComponent: JSX.Element,
-    planningList?: IObjectPlanningItem[]
+  FilterComponent: JSX.Element;
+  planningList?: IObjectPlanningItem[];
 }
 
-const Planning: React.FC<Props> = ({ FilterComponent, planningList }) => {
+const Planning: React.FC<Props> = ({ planningList }) => {
+  const { resolution } = useAdaptiveContext();
 
-    const [sort, setSort] = useState('default')
+  const [sort, setSort] = useState("default");
 
-    let list: any = []
-    if(sort === 'default'){
-        list = planningList?.sort((a: any, b: any) => a.id > b.id ? 1 : -1)
-    } else if(sort === 'bigger'){
-        list = planningList?.sort((a: any, b: any) => a.price > b.price ? 1 : -1)
-    } else if(sort === 'smaller'){
-        list = planningList?.sort((a: any, b: any) => a.price < b.price ? 1 : -1)
-    }
-
-    return (
-        <div className={s.container}>
-            <HeadLine title="Квартиры и аппартаменты">
-                <div className={s.filterWrapper}>
-                    <PlanningFilter sort={sort} setSort={setSort}/>
-                </div>
-                <div className={s.content}>
-                    {planningList && planningList.map(({ file, price, name, buildingNumber, deadline, floor, id }, idx) =>
-                        <Link href={`/apartment/${id}`} key={id}>
-                            <a className={s.link}>
-                                <Card 
-                                    key={idx} 
-                                    image={file ? file[0].url : ''} 
-                                    price={price} 
-                                    title={name} 
-                                    housing={buildingNumber} 
-                                    deadline={datetoQuarterFormat(deadline)} 
-                                    floor={floor} 
-                                />
-                            </a>
-                        </Link>
+  return (
+    <div className={s.container}>
+      <HeadLine title="Квартиры и аппартаменты">
+        <DesktopOnly>
+          <div className={s.filterWrapper}>
+            <PlanningFilter sort={sort} setSort={setSort} />
+          </div>
+        </DesktopOnly>
+        <div className={s.content}>
+          {planningList &&
+            planningList.map(
+              (
+                { file, price, name, buildingNumber, deadline, floor, id },
+                idx
+              ) =>
+                resolution <= AdaptiveParams.KT2 ? (
+                  <React.Fragment key={id}>
+                    {idx <= 1 && (
+                      <Link href={`/apartment/${id}`}>
+                        <a className={s.link}>
+                          <Card
+                            key={idx}
+                            image={file ? file[0]?.url : ""}
+                            price={price}
+                            title={name}
+                            housing={buildingNumber}
+                            deadline={datetoQuarterFormat(deadline)}
+                            floor={floor}
+                          />
+                        </a>
+                      </Link>
                     )}
-                </div>
-            </HeadLine>
+                  </React.Fragment>
+                ) : (
+                  <Link href={`/apartment/${id}`} key={id}>
+                    <a className={s.link}>
+                      <Card
+                        key={idx}
+                        image={file ? file[0]?.url : ""}
+                        price={price}
+                        title={name}
+                        housing={buildingNumber}
+                        deadline={datetoQuarterFormat(deadline)}
+                        floor={floor}
+                      />
+                    </a>
+                  </Link>
+                )
+            )}
         </div>
-    )
-}
+        <MobileOnly>
+          <div className={s.mobileButtonWrapper}>
+            <BaseButton className={s.button} type="primary">
+              Показать объявления
+            </BaseButton>
+          </div>
+        </MobileOnly>
+      </HeadLine>
+    </div>
+  );
+};
 
-export default Planning
+export default Planning;
