0afe6ca30750a5a1329932ca57157d632b528d03
Maksim <kylfyk@bk.ru>
2022-05-05 20:57:50 +0500

    Adaptive objects pages

--- src/components/containers/Planning/Card/Card.tsx
+++ src/components/containers/Planning/Card/Card.tsx
@@ -1,54 +1,70 @@
-import s from './Card.module.scss'
-import Image from 'next/image'
-import React from 'react'
-import Typography from '../../../shared/Typography/Typography'
-import { formatNumbersToCurrency } from '../../../../utils/general'
+import s from "./Card.module.scss";
+import Image from "next/image";
+import React from "react";
+import Typography from "../../../shared/Typography/Typography";
+import { formatNumbersToCurrency } from "../../../../utils/general";
 
 interface Props {
-    image: any,
-    price: number,
-    title: string,
-    housing: number,
-    floor: number,
-    deadline: string | undefined
+  image: any;
+  price: number;
+  title: string;
+  housing: number;
+  floor: number;
+  deadline: string | undefined;
 }
 
-const Card: React.FC<Props> = ({ image, price, title, housing, floor, deadline }) => {
-
-    return (
-        <div className={s.wrapper}>
-            <div className={s.imgContainer}>
-                {image && <Image 
-                            loader={() => image} 
-                            unoptimized src={image} 
-                            layout={'fill'} 
-                            alt="Planning image" 
-                            className={s.img} 
-                        />
-                }
-            </div>
-            <div className={s.content}>
-                <div className={s.contentRow}>
-                    <Typography weight="bold">{title}</Typography>
-                    <Typography weight="bold" color="nude" className={s.price}>{formatNumbersToCurrency(price)} ₽</Typography>
-                </div>
-                <div className={s.contentRow}>
-                    <div>
-                        <Typography weight="light" color="tertiary">Корпус</Typography>
-                        <Typography>{housing}</Typography>
-                    </div>
-                    <div>
-                        <Typography weight="light" color="tertiary">Этаж</Typography>
-                        <Typography>{floor}</Typography>
-                    </div>
-                    <div>
-                        <Typography weight="light" color="tertiary">Срок сдачи</Typography>
-                        <Typography>{deadline}</Typography>
-                    </div>
-                </div>
-            </div>
+const Card: React.FC<Props> = ({
+  image,
+  price,
+  title,
+  housing,
+  floor,
+  deadline,
+}) => {
+  return (
+    <div className={s.wrapper}>
+      <div className={s.imgContainer}>
+        {image && (
+          <Image
+            loader={() => image}
+            unoptimized
+            src={image}
+            layout={"fill"}
+            alt="Planning image"
+            className={s.img}
+          />
+        )}
+      </div>
+      <div className={s.content}>
+        <div className={s.contentRow}>
+          <Typography weight="bold">{title}</Typography>
+          <Typography weight="bold" color="nude" className={s.price}>
+            {formatNumbersToCurrency(price)} ₽
+          </Typography>
         </div>
-    )
-}
+        <div className={s.contentRow}>
+          <div>
+            <Typography weight="light" color="tertiary">
+              Корпус
+            </Typography>
+            <Typography>{housing}</Typography>
+          </div>
+          <div>
+            <Typography weight="light" color="tertiary">
+              Этаж
+            </Typography>
+            <Typography>{floor}</Typography>
+          </div>
+          <div>
+            <Typography weight="light" color="tertiary">
+              Срок сдачи
+            </Typography>
+            <Typography>{deadline}</Typography>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
--- src/components/shared/AdressEstate/AdressEstate.tsx
+++ src/components/shared/AdressEstate/AdressEstate.tsx
@@ -1,23 +1,27 @@
-import React, { ReactNode } from 'react';
-import s from './AdressEstate.module.scss'
+import React, { ReactNode } from "react";
+import s from "./AdressEstate.module.scss";
 import Typography from "../Typography/Typography";
 
 type AdressEstatePropsType = {
-    item: string
-    text?: string
-    children?: ReactNode,
-    variant?: "primary" | "secondary",
-    className?: string,
-}
-
-export const AdressEstate: React.FC<AdressEstatePropsType> = ({ className, item, text, children, variant = "primary" }) => {
-
-    return (
-        <div className={s.blockAdvantage}>
-            <Typography className={s.item} size={'small'} weight={'light'}>
-                {item}
-            </Typography>
-        </div>
-    );
+  item: string;
+  text?: string;
+  children?: ReactNode;
+  variant?: "primary" | "secondary";
+  className?: string;
 };
 
+export const AdressEstate: React.FC<AdressEstatePropsType> = ({
+  className,
+  item,
+  text,
+  children,
+  variant = "primary",
+}) => {
+  return (
+    <div className={s.blockAdvantage}>
+      <Typography className={s.item} size={"small"} weight={"light"}>
+        {item}
+      </Typography>
+    </div>
+  );
+};
