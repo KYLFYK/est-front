2242f7d93e09b7e16216571fe300202a00bde4a5
Aleksandr <zybkob@f-case.ru>
2022-05-06 10:51:49 +0300

    ConstructProgress 320px

--- src/components/containers/ConstructProgress/ConstructProgress.tsx
+++ src/components/containers/ConstructProgress/ConstructProgress.tsx
@@ -5,6 +5,7 @@ import "slick-carousel/slick/slick-theme.css";
 import BaseSlider from '../../shared/BaseSlider/BaseSlider';
 import {BaseDropDown} from '../../shared/BaseDropDown/BaseDropDown'
 import Typography from '../../shared/Typography/Typography';
+import {DesktopOnly} from "../Adaptive/DesktopOnly";
 
 interface Props {
     images: string[],
@@ -22,10 +23,20 @@ const ConstructProgress: React.FC<Props> = ({ images, info }) => {
                 </Typography>
                 <div className={s.df}>
                     <div className={s.sliderContainer}>
-                        <BaseSlider images={info[value].images && info[value].images.length ? info[value].images : images} height={295} withArrows withFavorite={false} onClickFavorite={() => {}}/>
+                        <BaseSlider
+                            images={info[value].images && info[value].images.length ? info[value].images : images}
+                            height={295}
+                            withArrows
+                            withFavorite={false}
+                            onClickFavorite={() => {}}
+                        />
                     </div>
                     <div className={s.infoContainer}>
-                        <BaseDropDown options={info} value={info[Number(value)].value} placeholder="Выбрерите опцию" onChange={(e) => {setValue(e)}} />
+                        <BaseDropDown
+                            options={info}
+                            value={info[Number(value)].value}
+                            placeholder="Выбрерите опцию"
+                            onChange={(e) => {setValue(e)}} />
                         <Typography>
                             {info[Number(value)].title}
                         </Typography>
